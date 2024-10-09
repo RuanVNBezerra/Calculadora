a5a0e5bd3116b57320f96c339173fa2759d1f8b1
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-10-09 20:25:05 -0300

    Atualização da calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,65 +6,98 @@ namespace Calculadora
     {
         static void Main()
         {
-            // variáveis usadas para o programa
+            // variáveis usadas no programa
             float numero1;
             float numero2;
             float resultado;
-            char operacoes;
+            char operaçoes;
+            string input;
 
             Console.WriteLine("BEM VINDO A SUA CALCULADORA!");
-            Console.WriteLine("Digite o primeiro numero: ");
-            while (!float.TryParse(Console.ReadLine(), out numero1)) // Enquanto oque for colocado for diferente de um numero, da erro e retorna a mesma pergunta até o usuario digitar um numero
-            {
-                Console.WriteLine("Entrada invalida para a calculadora, por favor insira um numero valido!");
-            }
-            Console.WriteLine("Digite o segundo numero: ");
-            while (!float.TryParse(Console.ReadLine(),out numero2)) // A mesma coisa acontece aqui, caso o usuario coloque algo que não seja um numero, entra em loop até sair um numero.
-            {
-                Console.WriteLine("Entrada invalida para a calculadora, por favor insira um numero valido!");
-            }
-            Console.WriteLine("Escolha o operador: (+ , - , * , / )");
-            operacoes = Console.ReadKey().KeyChar; // necessario para ler o caractere
 
-            Console.WriteLine(); // pular linha
+            while (true) // laço de repetição que mantem o programa rodando até o usuario decidir sair.
+            {
+                Console.WriteLine("Digite o primeiro numero: (Ou digite 'sair' para encerrar o programa) ");
+                input = Console.ReadLine();
+                if (input.ToLower() == "sair") // caso o usuario digite "sair" minusculo, o programa encerra.
+                {
+                    break;
+                }
+                while(!float.TryParse(input, out numero1)) // Enquanto oque o usuario não digitar um numero, o programa ficara em loop até um numero ser inserido.
+                {
+                    Console.WriteLine("Erro na entrada, por favor digite um numero valido");
+                    input = Console.ReadLine(); 
 
-            resultado = (numero1 + numero2);
-            /*
+                    if (input.ToLower() == "sair") // verifica denovo se o usuario deseja encerrar o programa 
+                    { 
+                        break;
+                    }
+                }
+                // O mesmo acontece ao pedir o segundo numero, caso o usuario digite "sair" o programa encerra.
+                Console.WriteLine("Digite o segundo numero: (Ou digite 'sair' para encerrar o programa)");
+                input = Console.ReadLine();
+                if (input.ToLower() == "sair")
+                {
+                    break;
+                }
+
+                while (!float.TryParse(input,out numero2))
+                {
+                    Console.WriteLine("Erro na entrada, por favor digite um numero valido");
+                    input = Console.ReadLine(); 
+                    if(input.ToLower() == "sair" )
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Selecione a operação que deseja (+ , - , * , /)");
+                operaçoes = Console.ReadKey().KeyChar; // le o caractere que o usuario digitar
+
+                Console.WriteLine(); // pular linha
+                
+              /*
              Aqui dependendo do caso que o usúario escolher 
              um desses casos abaixo irá acontecer
              */
-            switch (operacoes)
-            {
-                case '+': // caso de soma
-                    resultado = numero1 + numero2;
-                    Console.WriteLine($"O resultado de {numero1} + {numero2} é {resultado}");
-                break;
-                case '-': // caso de subtração
-                    resultado = numero1 - numero2; 
-                    Console.WriteLine($"O resultado {numero1} - {numero2} é {resultado}");
-                    break;
-                case '*': // caso de multiplicação
-                    resultado = numero1 * numero2;
-                    Console.WriteLine($"O resultado de {numero1} * {numero2} é {resultado}");
-                    break;
-                case '/': // caso de divisão
-                    if ( numero2 != 0 ) // uso de estrutura de decisão IF para ver se o segundo numero é difente de zero
-                    {
-                        resultado = numero1 / numero2;
-                        Console.WriteLine($"O resultado de {numero1} / {numero2} é {resultado}"); // se for, a divisão ocorrerá normalmente
-                    }
-                    else
-                    {
-                        Console.WriteLine("Numero divisivel por zero não pode!"); // se não, retorna erro
-                    }
-                    break;
-                    default:
-                    Console.WriteLine("Operação invalida!");
-                    break;  
+                
+                switch (operaçoes)
+                {
+                    case '+': // caso de soma
+                        resultado = numero1 + numero2;
+                        Console.WriteLine($"A soma de {numero1} + {numero2} é {resultado}");
+                        break;
+                    case '-': // caso de subtração
+                        resultado = numero1 - numero2;
+                        Console.WriteLine($"A subtração de {numero1} - {numero2} é {resultado}");
+                        break;
+                    case '*': // caso de multiplicação
+                        resultado = numero1 * numero2;
+                        Console.WriteLine($"A multiplicação de {numero1} * {numero2} é {resultado}");
+                        break;
+                    case '/': // caso de divisão
+                        if (numero2 != 0) // se o numero dois for diferente de zero, o calculo é feito normalmente
+                        {
+
+                            resultado = numero1 / numero2;
+                            Console.WriteLine($"A divisão de {numero1} / {numero2} é {resultado}");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("ERRO! numero divisivel por zero não é permitido");
+                        }
+                        break;
+                        default:
+                        Console.WriteLine("ERRO! operação invalida!");
+                        break;
+
+                }
+
+                Console.WriteLine(); // nova linha para a segundo calculo e em diante
             }
 
-            Console.WriteLine("Fim da Calculadora :)");
-            Console.ReadLine();
+            Console.WriteLine("FIM DA CALCULADORA :) ");
+            Console.WriteLine();
         }
     }
 }
